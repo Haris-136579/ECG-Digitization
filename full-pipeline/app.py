0aa09ffd4123f791cb5dc27b1274738bba68bc68
Muhammad Haris Khan <i222673@nu.edu.pk>
2025-08-08 11:07:33 +0500

    refactor: update server run configuration and add render.yaml for deployment

--- full-pipeline/app.py
+++ full-pipeline/app.py
@@ -135,6 +135,10 @@ def process_ecg():
         if os.path.exists(input_path):
             os.remove(input_path)
 
-# --- Run Server ---
+# # --- Run Server ---
+# if __name__ == '__main__':
+#     app.run(debug=False, port=5001)
+
 if __name__ == '__main__':
-    app.run(debug=False, port=5001)
+    port = int(os.environ.get("PORT", 5001))  # Render sets PORT dynamically
+    app.run(debug=False, host='0.0.0.0', port=port)
